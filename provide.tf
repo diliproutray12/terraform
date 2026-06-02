c5a6f05662508679ba8d044c6bdde5a387989f74
diliproutray12 <diliproutray8@gmail.com>
2026-06-03 02:12:41 +0530

    create a storage

--- provide.tf
+++ provide.tf
@@ -14,3 +14,12 @@ resource "azurerm_resource_group" "dilip" {
   name = "diliprg"
   location = "central india"
 }
+resource "azurerm_storage_account" "store" {
+  name = "storageaccount"
+  resource_group_name = azurerm_storage_account.dilip
+  location = azurerm_storage_account.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+
+  
+}
